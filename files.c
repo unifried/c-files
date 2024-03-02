e016899873706ed0eba0dcdda4f9ec203f1d5b7c
James <124284200+unifried@users.noreply.github.com>
2024-03-02 10:06:22 +0800

    1.0.2
    Fixed an issue where the file pointer would not be reset after reading the number of lines.

--- files.c
+++ files.c
@@ -1,7 +1,7 @@
 /*!
  * Author: James Carppe
  * Date: 29/02/2024
- * Version: 1.0.1
+ * Version: 1.0.2
  *
  * @brief Defines & implements types to simplify file input & output.
  *
@@ -9,28 +9,29 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "files.h"
 
 // Gets the number of lines in a given file
-int get_lines_in_file(FILE* target) {
+int get_lines_in_file(FILE *target) {
     // Count the lines
     int lines = 0;
 
-    int current_character = '';
-    while (current_character != EOF) {
+    int current_character;
+    do {
         // Read the next character in the file
         current_character = fgetc(target);
 
         // If the character is a new line charcter, add 1 to lines
         if (current_character == '\n') lines++;
-    }
+    } while (current_character != EOF);
 
     // Return lines
     return lines;
 }
 
 // Opens and returns a read-only file inside a ReadOnly_File struct
-struct ReadOnly_File open_readonly_file(const char* file_path) {
+struct ReadOnly_File open_readonly_file(const char *file_path) {
     // Open the file
     FILE *file = fopen(file_path, "r");
 
@@ -43,7 +44,7 @@ struct ReadOnly_File open_readonly_file(const char* file_path) {
 
         // Return a blank struct
         result.pointer = NULL;
-        result.lines   = 0;
+        result.lines = 0;
 
         return result;
     }
@@ -53,15 +54,19 @@ struct ReadOnly_File open_readonly_file(const char* file_path) {
     // Get the lines in the file
     const int lines = get_lines_in_file(file);
 
+    // Reset pointer
+    fclose(file);
+    file = fopen(file_path, "r");
+
     // Return a completed struct
     result.pointer = file;
-    result.lines   = lines;
+    result.lines = lines;
 
     return result;
 }
 
 // Opens and returns a read+write file inside a ReadWrite_File struct
-struct ReadWrite_File open_readwrite_file(const char* file_path) {
+struct ReadWrite_File open_readwrite_file(const char *file_path) {
     // Open the file
     FILE *file = fopen(file_path, "r+");
 
@@ -74,7 +79,7 @@ struct ReadWrite_File open_readwrite_file(const char* file_path) {
 
         // Return a blank struct
         result.pointer = NULL;
-        result.lines   = 0;
+        result.lines = 0;
 
         return result;
     }
@@ -84,9 +89,13 @@ struct ReadWrite_File open_readwrite_file(const char* file_path) {
     // Get the lines in the file
     const int lines = get_lines_in_file(file);
 
+    // Reset pointer
+    fclose(file);
+    file = fopen(file_path, "r");
+
     // Return a completed struct
     result.pointer = file;
-    result.lines   = lines;
+    result.lines = lines;
 
     return result;
 }
